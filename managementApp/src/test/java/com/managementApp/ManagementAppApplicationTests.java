fad1a7d023829942c3da56b13865310258f729c9
Benyi Cordero <benyi.uriel2006@gmail.com>
2025-06-24 17:09:50 -0600

    Creacion del proyecto

---
+++ managementApp/src/test/java/com/managementApp/ManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.managementApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
